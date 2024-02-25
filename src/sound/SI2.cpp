9c775dce401e97732117c2d8c364a721994dc83b
VDm <vdm-dev@users.noreply.github.com>
2024-02-25 18:00:12 +0400

    chore(sound): add SI2 CVars

--- src/sound/SI2.cpp
+++ src/sound/SI2.cpp
@@ -1,5 +1,6 @@
 #include "sound/SI2.hpp"
 #include "ui/FrameScript.hpp"
+#include "console/CVar.hpp"
 
 void SI2::RegisterScriptFunctions() {
     FrameScript_Method* item = s_ScriptFunctions;
@@ -10,5 +11,13 @@ void SI2::RegisterScriptFunctions() {
 }
 
 int32_t SI2::Init(int32_t flag) {
+
+    Log_Init();
+    SI2_LOG("=> Version %s (%s) %s", "1.0.0", "00000", "Feb 25 2024");
+    SI2_LOG(" ");
+    SI2_LOG("=> Setting up Game Sound:");
+    SI2_LOG(" - SESound Engine Init");
+    SI2_LOG(" - FMOD Memory Init");
+
     return 0;
 }
--- src/sound/SI2.hpp
+++ src/sound/SI2.hpp
@@ -8,6 +8,10 @@
 #include <fmod.h>
 #include <fmod_errors.h>
 
+
+#define SI2_ERR(errcode, format, ...) SI2::Log_Write(__LINE__, __FILE__, errcode, format, __VA_ARGS__)
+#define SI2_LOG(format, ...) SI2::Log_Write(__LINE__, __FILE__, FMOD_OK, format, __VA_ARGS__)
+
 class SI2 {
     public:
         // Static variables
@@ -20,6 +24,7 @@ class SI2 {
         static int32_t Log_Init();
         static void Log_Write(const char* format, ...);
         static void Log_Write(uint32_t line, const char* filename, FMOD_RESULT errcode, const char* format, ...);
+        static void RegisterCVars();
         static int32_t Init(int32_t flag);
 };
 
